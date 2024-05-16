8bb623231eca66358b8a60ff1e9323f2563fdac4
Marat <hellp8901@gmail.com>
2024-05-16 12:22:56 +0500

    updated models, constants

--- backend/core/constants/news.py
+++ backend/core/constants/news.py
@@ -1,4 +1,9 @@
+from typing import List
+
+
 LEN_TITLE: int = 150
-LEN_AGE_RESTRICTION: int = 10
 
 LEN_CATEGORY_NAME: int = 20
+
+MAX_FILE_SIZE: int = 10 * 1024 * 1024
+ALLOWED_EXTENSIONS: List[str] = []
