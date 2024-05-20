f3ea0f445131d8c88b9b2dee7d494d04c3153dfa
Marat <hellp8901@gmail.com>
2024-05-20 19:40:08 +0500

    fixed smtp

--- backend/user/tasks.py
+++ backend/user/tasks.py
@@ -1,14 +1,26 @@
+import logging
+
 from celery import shared_task
 from django.core.mail import send_mail
+from django.conf import settings
+
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def send_email_task(subject, message, recipient_list, html_message):
-    send_mail(
-        subject,
-        message,
-        'anarant91@gmail.com',
-        recipient_list,
-        html_message=html_message,
-        fail_silently=False,
-    )
+    logger.info(f':::::::::::::::::::{settings.EMAIL_HOST}')
+    logger.info(f':::::::::::::::::::{settings.EMAIL_PORT}')
+    try:
+        send_mail(
+            subject,
+            message,
+            'anarant91@gmail.com',
+            recipient_list,
+            html_message=html_message,
+            fail_silently=False,
+        )
+    except Exception as e:
+        logger.error(f'ERROR: {e}')
+        raise
